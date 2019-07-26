ce1ce1b614713599071f4924ed7ef547c3ff223b
孙涛章 <suntaozhang@aegis-data.cn>
2019-07-26 11:46:55 +0800

    测试bus总线

---
+++ consumer_client/src/main/java/com/sun/controller/Refresh.java
@@ -0,0 +1,23 @@
+package com.sun.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description
+ * @author: suntaozhang
+ * @create: 2019-07-26
+ * @since 0.0.1
+ **/
+@RestController
+public class Refresh {
+
+  @Value("${properties.name}")
+  private String name;
+
+  @GetMapping("/refresh")
+  public String testRefresh(){
+    return "refresh:" + name;
+  }
+}
