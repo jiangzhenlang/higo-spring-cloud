e5b953afff0611000ed23a24cadb95a087e60740
江振朗 <jiangzhenlang@aegis-data.cn>
2019-07-26 13:14:11 +0800

    修改消息总线

--- config_server/src/main/java/com/sun/ConfigServerApplication.java
+++ config_server/src/main/java/com/sun/ConfigServerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 刷新配置请求路径：http://localhost:1006/actuator/bus-refresh
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer
--- consumer_client/src/main/java/com/sun/controller/Refresh.java
+++ consumer_client/src/main/java/com/sun/controller/Refresh.java
@@ -1,6 +1,7 @@
 package com.sun.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2019-07-26
  * @since 0.0.1
  **/
+@RefreshScope
 @RestController
 public class Refresh {
 
